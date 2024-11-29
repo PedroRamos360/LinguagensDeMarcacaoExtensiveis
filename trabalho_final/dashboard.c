7f8890fb16e9f5cdd1c3ba854e775f7ed832caff
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-11-28 21:27:17 -0300

    feat: general screen and NFs screen

--- trabalho_final/dashboard.c
+++ trabalho_final/dashboard.c
@@ -1,4 +1,6 @@
 #include "dashboard.h"
+#include "xml_to_json.h"
+#include "validate_json.h"
 
 static void on_back_button_clicked(GtkButton *button, gpointer user_data)
 {
@@ -6,35 +8,263 @@ static void on_back_button_clicked(GtkButton *button, gpointer user_data)
     gtk_stack_set_visible_child_name(stack, "main-view");
 }
 
-// Helper function to create and add a "General" tab
 void add_general_view(GtkNotebook *notebook)
 {
-    GtkWidget *general_view = gtk_label_new("General View Content");
+    GtkWidget *general_view = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_notebook_append_page(notebook, general_view, gtk_label_new("General"));
+
+    char *nfe_files[] = {
+        xml_to_json(read_file("notas/NFE1.xml")),
+        xml_to_json(read_file("notas/NFE2.xml")),
+        xml_to_json(read_file("notas/NFE3.xml")),
+        xml_to_json(read_file("notas/NFE4.xml")),
+        xml_to_json(read_file("notas/NFE5.xml")),
+        xml_to_json(read_file("notas/NFE6.xml"))};
+
+    int total_nfs = 0;
+    int total_products = 0;
+    double total_value = 0.0;
+    double total_taxes = 0.0;
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (!nfe_files[i])
+        {
+            fprintf(stderr, "Error converting XML to JSON for NFE%d\n", i + 1);
+            continue;
+        }
+
+        json_error_t error;
+        json_t *root = json_loads(nfe_files[i], 0, &error);
+        if (!root)
+        {
+            fprintf(stderr, "Error parsing JSON for NFE%d: %s\n", i + 1, error.text);
+            free(nfe_files[i]);
+            continue;
+        }
+
+        json_t *nfeProc = json_object_get(root, "nfeProc");
+        if (nfeProc)
+        {
+            json_t *NFe = json_object_get(nfeProc, "NFe");
+            json_t *infNFe = json_object_get(NFe, "infNFe");
+            total_nfs++;
+            json_t *det = json_object_get(infNFe, "det");
+            if (json_is_array(det))
+            {
+                total_products += json_array_size(det);
+            }
+            else
+            {
+                total_products += 1;
+            }
+            json_t *pag = json_object_get(infNFe, "pag");
+            json_t *detPag = json_object_get(pag, "detPag");
+            json_t *vPag = json_object_get(detPag, "vPag");
+            json_t *vpag_text = json_object_get(vPag, "#text");
+            total_value += atof(json_string_value(vpag_text));
+            json_t *total = json_object_get(infNFe, "total");
+            json_t *ICMSTot = json_object_get(total, "ICMSTot");
+            json_t *vTotTrib = json_object_get(ICMSTot, "vTotTrib");
+            json_t *vicms_text = json_object_get(vTotTrib, "#text");
+            total_taxes += atof(json_string_value(vicms_text));
+        }
+
+        json_decref(root);
+        free(nfe_files[i]);
+    }
+
+    GtkWidget *label_nfs = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_nfs), g_strdup_printf("<span font='30'>Total NFs: %d</span>", total_nfs));
+    gtk_box_pack_start(GTK_BOX(general_view), label_nfs, FALSE, FALSE, 0);
+
+    GtkWidget *label_products = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_products), g_strdup_printf("<span font='30'>Total Products: %d</span>", total_products));
+    gtk_box_pack_start(GTK_BOX(general_view), label_products, FALSE, FALSE, 0);
+
+    GtkWidget *label_value = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_value), g_strdup_printf("<span font='30'>Total Value: %.2f</span>", total_value));
+    gtk_box_pack_start(GTK_BOX(general_view), label_value, FALSE, FALSE, 0);
+
+    GtkWidget *label_taxes = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_taxes), g_strdup_printf("<span font='30'>Total Taxes: %.2f</span>", total_taxes));
+    gtk_box_pack_start(GTK_BOX(general_view), label_taxes, FALSE, FALSE, 0);
+
+    gtk_widget_show_all(general_view);
 }
 
-// Helper function to create and add an "NFs" tab
+void display_nfe_data(const char *nfe_json)
+{
+    json_error_t error;
+    json_t *root = json_loads(nfe_json, 0, &error);
+    if (!root)
+    {
+        fprintf(stderr, "Error parsing JSON: %s\n", error.text);
+        return;
+    }
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "NFE Details");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 600);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    json_t *nfeProc = json_object_get(root, "nfeProc");
+    json_t *NFe = json_object_get(nfeProc, "NFe");
+    json_t *infNFe = json_object_get(NFe, "infNFe");
+    json_t *emit = json_object_get(infNFe, "emit");
+    const char *store_name = "N/A";
+    if (emit)
+    {
+        json_t *xNome = json_object_get(emit, "xNome");
+        json_t *xnome_text = json_object_get(xNome, "#text");
+        if (xnome_text && json_is_string(xnome_text))
+        {
+            store_name = json_string_value(xnome_text);
+        }
+    }
+    GtkWidget *label_store_title = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_store_title), "<b>Store:</b>");
+    gtk_box_pack_start(GTK_BOX(vbox), label_store_title, FALSE, FALSE, 0);
+
+    GtkWidget *label_store = gtk_label_new(NULL);
+    gtk_label_set_text(GTK_LABEL(label_store), store_name);
+    gtk_box_pack_start(GTK_BOX(vbox), label_store, FALSE, FALSE, 0);
+
+    GtkWidget *label_products_title = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_products_title), "<b>Products:</b>");
+    gtk_box_pack_start(GTK_BOX(vbox), label_products_title, FALSE, FALSE, 0);
+
+    json_t *det = json_object_get(infNFe, "det");
+    if (json_is_array(det))
+    {
+        size_t index;
+        json_t *item;
+        json_array_foreach(det, index, item)
+        {
+            json_t *prod = json_object_get(item, "prod");
+
+            const char *product_name = "N/A";
+            double product_value = 0.0;
+
+            json_t *xProd = json_object_get(prod, "xProd");
+            json_t *xprod_text = json_object_get(xProd, "#text");
+            if (xprod_text && json_is_string(xprod_text))
+            {
+                product_name = json_string_value(xprod_text);
+            }
+
+            json_t *vProd = json_object_get(prod, "vProd");
+            json_t *vprod_text = json_object_get(vProd, "#text");
+            if (vprod_text && json_is_string(vprod_text))
+            {
+                product_value = atof(json_string_value(vprod_text));
+            }
+
+            GtkWidget *product_label = gtk_label_new(NULL);
+            gtk_label_set_text(GTK_LABEL(product_label),
+                               g_strdup_printf("- %s: %.2f", product_name, product_value));
+            gtk_box_pack_start(GTK_BOX(vbox), product_label, FALSE, FALSE, 0);
+        }
+    }
+    else
+    {
+        json_t *prod = json_object_get(det, "prod");
+        const char *product_name = "N/A";
+        double product_value = 0.0;
+
+        json_t *xProd = json_object_get(prod, "xProd");
+        json_t *xprod_text = json_object_get(xProd, "#text");
+        if (xprod_text && json_is_string(xprod_text))
+        {
+            product_name = json_string_value(xprod_text);
+        }
+
+        json_t *vProd = json_object_get(prod, "vProd");
+        json_t *vprod_text = json_object_get(vProd, "#text");
+        if (vprod_text && json_is_string(vprod_text))
+        {
+            product_value = atof(json_string_value(vprod_text));
+        }
+
+        GtkWidget *product_label = gtk_label_new(NULL);
+        gtk_label_set_text(GTK_LABEL(product_label),
+                           g_strdup_printf("- %s: %.2f", product_name, product_value));
+        gtk_box_pack_start(GTK_BOX(vbox), product_label, FALSE, FALSE, 0);
+    }
+
+    double total_value = 0.0;
+    json_t *pag = json_object_get(infNFe, "pag");
+
+    json_t *detPag = json_object_get(pag, "detPag");
+
+    json_t *vPag = json_object_get(detPag, "vPag");
+    json_t *vpag_text = json_object_get(vPag, "#text");
+    if (vpag_text && json_is_string(vpag_text))
+    {
+        total_value = atof(json_string_value(vpag_text));
+    }
+
+    GtkWidget *label_total_title = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label_total_title), "<b>Total Value:</b>");
+    gtk_box_pack_start(GTK_BOX(vbox), label_total_title, FALSE, FALSE, 0);
+
+    GtkWidget *label_total = gtk_label_new(NULL);
+    gtk_label_set_text(GTK_LABEL(label_total), g_strdup_printf("%.2f", total_value));
+    gtk_box_pack_start(GTK_BOX(vbox), label_total, FALSE, FALSE, 0);
+
+    json_decref(root);
+    gtk_widget_show_all(window);
+}
+
+void on_nfe_button_clicked(GtkButton *button, gpointer user_data)
+{
+    const char *nfe_json = (const char *)user_data;
+    display_nfe_data(nfe_json);
+}
+
 void add_nf_view(GtkNotebook *notebook)
 {
-    GtkWidget *nf_view = gtk_label_new("NF View Content");
+    GtkWidget *nf_view = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_notebook_append_page(notebook, nf_view, gtk_label_new("NFs"));
+
+    char *nfe_files[] = {
+        xml_to_json(read_file("notas/NFE1.xml")),
+        xml_to_json(read_file("notas/NFE2.xml")),
+        xml_to_json(read_file("notas/NFE3.xml")),
+        xml_to_json(read_file("notas/NFE4.xml")),
+        xml_to_json(read_file("notas/NFE5.xml")),
+        xml_to_json(read_file("notas/NFE6.xml"))};
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (!nfe_files[i])
+        {
+            fprintf(stderr, "Error converting XML to JSON for NFE%d\n", i + 1);
+            continue;
+        }
+
+        GtkWidget *button = gtk_button_new_with_label(g_strdup_printf("Open NFE %d", i + 1));
+        g_signal_connect(button, "clicked", G_CALLBACK(on_nfe_button_clicked), nfe_files[i]);
+        gtk_box_pack_start(GTK_BOX(nf_view), button, FALSE, FALSE, 0);
+    }
+
+    gtk_widget_show_all(nf_view);
 }
 
-// Helper function to create and add a "Taxes" tab
 void add_taxes_view(GtkNotebook *notebook)
 {
     GtkWidget *taxes_view = gtk_label_new("Taxes View Content");
     gtk_notebook_append_page(notebook, taxes_view, gtk_label_new("Taxes"));
 }
 
-// Helper function to create and add a "Suppliers" tab
 void add_suppliers_view(GtkNotebook *notebook)
 {
     GtkWidget *suppliers_view = gtk_label_new("Suppliers View Content");
     gtk_notebook_append_page(notebook, suppliers_view, gtk_label_new("Suppliers"));
 }
 
-// Helper function to create and add a "Transporters" tab
 void add_transporters_view(GtkNotebook *notebook)
 {
     GtkWidget *transporters_view = gtk_label_new("Transporters View Content");
--- trabalho_final/xml_to_json.h
+++ trabalho_final/xml_to_json.h
@@ -1,73 +1,14 @@
+#ifndef XML_TO_JSON_H
+#define XML_TO_JSON_H
+
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 #include <jansson.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-void add_attributes_to_json(xmlNode *node, json_t *json_obj)
-{
-    xmlAttr *attr;
-    for (attr = node->properties; attr; attr = attr->next)
-    {
-        char attr_name[256];
-        snprintf(attr_name, sizeof(attr_name), "@%s", attr->name);
-        json_object_set_new(json_obj, attr_name, json_string((const char *)attr->children->content));
-    }
-}
+void add_attributes_to_json(xmlNode *node, json_t *json_obj);
 
-void traverse_node(xmlNode *node, json_t *json_obj)
-{
-    for (xmlNode *cur_node = node; cur_node; cur_node = cur_node->next)
-    {
-        if (cur_node->type == XML_ELEMENT_NODE)
-        {
-            json_t *child_obj = json_object();
-            add_attributes_to_json(cur_node, child_obj);
-            if (cur_node->children && cur_node->children->type == XML_TEXT_NODE && cur_node->children->content[0] != '\n')
-            {
-                json_object_set_new(child_obj, "#text", json_string((const char *)cur_node->children->content));
-            }
-            const char *name = (const char *)cur_node->name;
-            json_t *existing_value = json_object_get(json_obj, name);
-            if (existing_value)
-            {
-                json_t *previous_value = json_object_get(json_obj, name);
-                if (!json_is_array(previous_value))
-                {
-                    json_t *array = json_array();
-                    json_array_append(array, previous_value);
-                    json_array_append(array, child_obj);
-                    json_object_set(json_obj, name, array);
-                }
-                else
-                {
-                    json_array_append(previous_value, child_obj);
-                }
-            }
-            else
-            {
-                json_object_set_new(json_obj, name, child_obj);
-            }
-            traverse_node(cur_node->children, child_obj);
-        }
-    }
-}
+void traverse_node(xmlNode *node, json_t *json_obj);
 
-char *xml_to_json(const char *xml_str)
-{
-    xmlDoc *doc = xmlReadMemory(xml_str, strlen(xml_str), "noname.xml", NULL, 0);
-    if (doc == NULL)
-    {
-        fprintf(stderr, "Failed to parse XML\n");
-        return NULL;
-    }
-    xmlNode *root_element = xmlDocGetRootElement(doc);
-    json_t *json_obj = json_object();
-    traverse_node(root_element, json_obj);
-    char *result = json_dumps(json_obj, JSON_INDENT(4));
-    json_decref(json_obj);
-    xmlFreeDoc(doc);
-    xmlCleanupParser();
-    return result;
-}
+char *xml_to_json(const char *xml_str);
+
+#endif // XML_TO_JSON_H
